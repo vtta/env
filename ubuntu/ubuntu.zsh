f55988c8029b6f80d72240927ebda76785991f84
Junliang HU <jlhu@cse.cuhk.edu.hk>
2021-09-28 00:45:07 +0800

    Auto install zig compiler in zshrc

--- ubuntu/ubuntu.zsh
+++ ubuntu/ubuntu.zsh
@@ -1,36 +1,45 @@
-LANG=C.UTF-8
+PKG_CONFIG_PATH="$HOME/.local/share/pkgconfig"
+LD_LIBRARY_PATH="/opt/local/lib"
+# LD_LIBRARY_PATH="/opt/local/lib:/opt/intel/oneapi/tbb/latest/lib/intel64/gcc4.8"
+RUSTFLAGS="-L /opt/local/lib"
+LANG=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
 XZ_OPT="-6eT0"
-PATH="$HOME/.local/bin:$PATH"
+PATH="$HOME/.local/bin:/opt/local/bin:$PATH"
 FZF_DEFAULT_COMMAND="fd --type file --color=always --follow --hidden --exclude .git"
 FZF_DEFAULT_OPTS="--ansi"
-export  PATH LANG XZ_OPT \
-    FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
+export PATH \
+  LANG LC_CTYPE LC_ALL \
+  FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS \
+  XZ_OPT \
+  PKG_CONFIG_PATH LD_LIBRARY_PATH
 alias vi="nvim"
 alias vim="nvim"
-
 
 ######## plugin starts
 # install zinit if not found
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+  command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+  command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" &&
+    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" ||
+    print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 # load zinit
 source "$HOME/.zinit/bin/zinit.zsh"
 # required by turbo mode plugins
 zinit light-mode for \
   zinit-zsh/z-a-bin-gem-node \
-  zinit-zsh/z-a-rust
+  zinit-zsh/z-a-rust \
+  zinit-zsh/z-a-readurl
 # theme
-zinit ice compile="(pure|async).zsh" pick="async.zsh" src="pure.zsh"
-zinit light sindresorhus/pure
+zinit light-mode for \
+  compile="(pure|async).zsh" pick="async.zsh" src="pure.zsh" sindresorhus/pure
 # completion + highlighting
 zinit wait lucid for \
- atload="!_zsh_autosuggest_start" \
-    zsh-users/zsh-autosuggestions
+  atload="!_zsh_autosuggest_start" \
+  zsh-users/zsh-autosuggestions
 zinit wait lucid blockf atpull="zinit creinstall -q ." for \
   zsh-users/zsh-completions \
   pkulev/zsh-rustup-completion
@@ -40,29 +49,35 @@ zinit wait lucid as=null from=gh-r for \
   sbin="*/fd" @sharkdp/fd \
   sbin="*/bat" @sharkdp/bat \
   sbin="*/exa" ogham/exa \
-  sbin="* -> ffmpeg" bpick="$(uname -s)-x64" eugeneware/ffmpeg-static \
+  sbin="marp" marp-team/marp-cli \
+  sbin="*/rga*" phiresky/ripgrep-all \
+  sbin="* -> ffmpeg" bpick="$(uname -s)-$(uname -m | sed s/x86_64/x64/g)" eugeneware/ffmpeg-static \
+  sbin="* -> jq" stedolan/jq \
   sbin="src" sourcegraph/src-cli \
-  id-as"fzf-bin" sbin"fzf" junegunn/fzf
+  id-as="fzf-bin" sbin="fzf" junegunn/fzf
 # uitls
 zinit wait lucid for \
   vtta/zsh-aliases-exa \
   ael-code/zsh-colored-man-pages \
   OMZP::git \
   multisrc"shell/{completion,key-bindings}.zsh" junegunn/fzf
-  #Aloxaf/fzf-tab
+#Aloxaf/fzf-tab
+zinit id-as=zig as="readurl|program" extract \
+  dlink="https://ziglang.org/builds/zig-$(uname -s | tr \[:upper:\] \[:lower:\] | sed s/darwin/macos/g)-x86_64-%VERSION%-dev.{10,20}.tar.xz" \
+  sbin="*/zig" \
+  for https://ziglang.org/download/
 # A little more complex rustup configuration that uses Bin-Gem-Node annex
 # and installs the cargo completion provided with rustup, using for-syntax
 zinit wait lucid id-as=rust as=null sbin="bin/*" rustup \
-    atload="[[ ! -f ${ZINIT[COMPLETIONS_DIR]}/_cargo ]] && zi creinstall rust; \
+  atload="[[ ! -f ${ZINIT[COMPLETIONS_DIR]}/_cargo ]] && zi creinstall rust; \
     export CARGO_HOME=\$PWD RUSTUP_HOME=\$PWD/rustup; \
     export PATH=\$CARGO_HOME/bin:\$PATH" for \
-        zdharma/null
+  zdharma/null
 # should be the last one to load
 zinit wait=1 lucid for \
- atinit="ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
-    zdharma/fast-syntax-highlighting
+  atinit="ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+  zdharma/fast-syntax-highlighting
 ######## plugin ends
-
 
 ######## histroy files
 HISTFILE="$HOME/.zsh_history"
@@ -77,10 +92,8 @@ setopt hist_verify            # show command with history expansion to user befo
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
 
-
 ######## SHORTCUTS
-bindkey "^[[1;5D" backward-word # ctrl + arrow left
-bindkey "^[[1;5C" forward-word # ctrl + arrow right
+bindkey "^[[1;5D" backward-word  # ctrl + arrow left
+bindkey "^[[1;5C" forward-word   # ctrl + arrow right
 bindkey "^[[H" beginning-of-line # home
-bindkey "^[[F" end-of-line # end
-
+bindkey "^[[F" end-of-line       # end
